ebe91d452b45e4d40859ce69515782fd33af79c3
⛅ Arnab Deka 🍭 <arnab.deka@gmail.com>
2022-10-05 16:50:28 -0700

    display details of meals in meals card

--- lib/widgets/list_meal_item.dart
+++ lib/widgets/list_meal_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../models/meal.dart';
 
@@ -33,8 +34,58 @@ class ListMealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
-                )
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    color: Colors.black54,
+                    child: Text(
+                      meal.title,
+                      style: const TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.schedule),
+                      const SizedBox(width: 5,),
+                      Text('${meal.duration} min')
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.work),
+                      const SizedBox(width: 5,),
+                      Text(toBeginningOfSentenceCase(meal.complexity.name)!),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.attach_money),
+                      const SizedBox(width: 5,),
+                      Text(toBeginningOfSentenceCase(meal.affordability.name)!),
+                    ],
+                  ),
+                ],
+              ),
             )
           ],
         ),
